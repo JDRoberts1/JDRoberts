1afb631bb3a115aa034391c28818bcf9aadf1ac8
JDRoberts1 <58571944+JDRoberts1@users.noreply.github.com>
2021-01-04 11:20:48 -0600

    Add Code exercise files to repo

---
+++ JeanaiRoberts_FinalProject/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JeanaiRoberts_FinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            HandleClientWindowSize();
+        }
+
+        void HandleClientWindowSize()
+
+        {
+
+            //Modify ONLY these float values
+
+            float HeightValueToChange = 1.4f;
+
+            float WidthValueToChange = 6.0f;
+
+            //DO NOT MODIFY THIS CODE
+
+            int height = Convert.ToInt32(Screen.PrimaryScreen.WorkingArea.Size.Height / HeightValueToChange);
+
+            int width = Convert.ToInt32(Screen.PrimaryScreen.WorkingArea.Size.Width / WidthValueToChange);
+
+            if (height < Size.Height)
+
+                height = Size.Height;
+
+            if (width < Size.Width)
+
+                width = Size.Width;
+
+            this.Size = new Size(width, height);
+
+            //this.Size = new Size(376, 720);
+
+        }
+    }
+}
